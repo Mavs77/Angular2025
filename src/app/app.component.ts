5770934bf53333e7e21514ab16f1a4e3c5a57ee8
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2025-01-29 20:25:04 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketComponent } from './dashboard/ticket/ticket.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketComponent,
+    DashboardItemComponent,
+  ],
+})
+export class AppComponent {}
---
+++ src/app/dashboard/ticket/ticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
+
+@Component({
+  selector: 'app-ticket',
+  imports: [NewTicketComponent],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css',
+})
+export class TicketComponent {}
